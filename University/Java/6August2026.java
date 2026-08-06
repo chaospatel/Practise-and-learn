d3e310761856e9b6811966348c9d2a2d5cd7365a
Dhyey Patel <chaospatel@users.noreply.github.com>
2026-08-06 13:29:09 +0530

    v1.1

---
+++ University/Java/6August2026.java
@@ -0,0 +1,8 @@
+class Family{
+    public static void main(String[] args){
+        Family father=new Family();
+        Family mother=new Family();
+        Family myself=new Family();
+        
+    }
+}
