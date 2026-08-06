914b0bc4e4ff27a2a8f732324f4c00a3cbcf111a
Dhyey Patel <chaospatel@users.noreply.github.com>
2026-08-06 13:51:36 +0530

    v1.4

--- University/Java/Family.java
+++ University/Java/Family.java
@@ -11,7 +11,7 @@ class Family{
         myself.age=20;
         myself.name="John"; 
         myself.bloodgroup="O+";
-        System.out.println("My Details");
+        System.out.println("---My Details---");
         System.out.println(myself.age);
         System.out.println(myself.name);
         System.out.println(myself.bloodgroup);
@@ -19,7 +19,7 @@ class Family{
         father.age=50;
         father.name="Robert";
         father.bloodgroup="A+";
-        System.out.println("Father's Details");
+        System.out.println("---Father's Details---");
         System.out.println(father.age);
         System.out.println(father.name);
         System.out.println(father.bloodgroup);
@@ -28,7 +28,7 @@ class Family{
         mother.age=48;
         mother.name="Mary";
         mother.bloodgroup="B+";
-        System.out.println("Mother's Details");
+        System.out.println("---Mother's Details---");
         System.out.println(mother.age);
         System.out.println(mother.name);
         System.out.println(mother.bloodgroup);
@@ -36,7 +36,7 @@ class Family{
         brother.age=18;
         brother.name="David";
         brother.bloodgroup="AB+";
-        System.out.println("Brother's Details");
+        System.out.println("---Brother's Details---");
         System.out.println(brother.age);
         System.out.println(brother.name);
         System.out.println(brother.bloodgroup);
