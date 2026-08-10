440840f5f35f66f0f0548ea2da3d2e1137371227
Dhyey Patel <chaospatel@users.noreply.github.com>
2026-08-10 19:41:19 +0530

    v1.1

---
+++ C++/console calculator.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+int main(){
+    char op;
+    double num1, num2;
+    double result;
+    std::cout << "Enter operator (+, -, *, /): ";
+    std::cin >> op;
+    std::cout << "Enter either (+ - / *)";
+    std::cin>>op;
+    std::cout << "Enter #1 ";
+    std::cin >> num1;
+    std::cout << "Enter #2";
+    std::cin >> num2;
+    switch(op){
+        case '+':
+            result=num1+num2;
+            std::cout <<result;
+            break;
+        case '-':
+            result=num1-num2;
+            std::cout <<result;
+            break;
+        case '*':
+            result=num1*num2;
+            std::cout <<result;
+            break;  
+        case '/':
+            result=num1/num2;
+            std::cout <<result;
+            break:
+        default:
+            std::cout << "Error! operator is not correct";
+            break;
+    }
+    std::cout << "-----------------------------------------------"
+    return 0;
+}
