1043a0e59e49c0ce9624eeb1c8283ee56a15d42e
Dhyey Patel <chaospatel@users.noreply.github.com>
2026-08-10 19:47:47 +0530

    v1.2

--- C++/calculator.cpp
+++ C++/calculator.cpp
@@ -3,35 +3,33 @@ int main(){
     char op;
     double num1, num2;
     double result;
-    std::cout << "Enter operator (+, -, *, /): ";
-    std::cin >> op;
     std::cout << "Enter either (+ - / *)";
     std::cin>>op;
     std::cout << "Enter #1 ";
     std::cin >> num1;
-    std::cout << "Enter #2";
+    std::cout << "Enter #2" ;
     std::cin >> num2;
     switch(op){
         case '+':
             result=num1+num2;
-            std::cout <<result;
+            std::cout <<result<<std::endl;
             break;
         case '-':
             result=num1-num2;
-            std::cout <<result;
+            std::cout <<result<<std::endl;
             break;
         case '*':
             result=num1*num2;
-            std::cout <<result;
+            std::cout <<result<<std::endl;
             break;  
         case '/':
             result=num1/num2;
-            std::cout <<result;
-            break:
-        default:
+            std::cout <<result<<std::endl;
+            break;
+        default :
             std::cout << "Error! operator is not correct";
             break;
     }
-    std::cout << "-----------------------------------------------"
+    std::cout << "-----------------------------------------------";
     return 0;
 }
